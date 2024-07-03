08e694cdba78fc44325a5548725f30777bd9c30c
Aleksandr Smelov <smelovalex14@gmail.com>
2024-07-03 13:54:10 +0300

    env added

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,18 +4,22 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET = 'dev-secret' } = process.env;
 
 module.exports = (req, res, next) => {
-  const jwtToken = req.cookies.jwt;
+  const jwtToken = req.cookies.jwt.replace('jwt=', '');
 
   let payload;
 
   try {
     payload = jwt.verify(jwtToken, JWT_SECRET);
     if (!payload) {
-      throw new UnauthorizedError('Необходима авторизация');
+      throw new UnauthorizedError(`${jwtToken}`);
     }
   } catch (err) {
     if (err.name === 'JsonWebTokenError') {
+<<<<<<< HEAD
       next(new UnauthorizedError('Необходима авторизация'));
+=======
+      throw new UnauthorizedError('плохой токен'); // Необходима авторизация
+>>>>>>> 216fc0263cdc987e698ead433b03d8a9b66f17e9
     }
     next(new UnauthorizedError('Произошла ошибка'));
   }
